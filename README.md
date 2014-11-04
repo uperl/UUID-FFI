d0e5fab51e43012bbd6747670b43ef9350375395
Graham Ollis <plicease@cpan.org>
2014-11-04 17:29:37 -0500

    doco

--- README.md
+++ README.md
@@ -2,6 +2,88 @@
 
 Universally Unique Identifiers FFI style
 
+# SYNOPSIS
+
+    my $uuid = UUID::FFI->new_random;
+    say $uuid->as_hex;
+
+# DESCRIPTION
+
+This module provides an FFI interface to `libuuid`.
+`libuuid` library is used to generate unique identifiers
+for objects that may be accessible beyond the local system
+
+# CONSTRUCTORS
+
+## new
+
+    my $uuid = UUID::FFI->new($hex);
+
+Create a new UUID object from the hex representation `$hex`.
+
+## new\_random
+
+    my $uuid = UUID::FFI->new_random;
+
+Create a new UUID object with a randomly generated value.
+
+## new\_time
+
+    my $uuid = UUID::FFI->new_time;
+
+Create a new UUID object generated using the time and mac address.
+This can leak information about when and where the UUID was generated.
+
+## new\_null
+
+    my $uuid = UUID::FFI->new_null;
+
+Create a new UUID `NULL UUID`  object (all zeros).
+
+# METHODS
+
+## is\_null
+
+    my $bool = $uuid->is_null;
+
+Returns true if the UUID is `NULL UUID`.
+
+## clone
+
+    my $uuid2 = $uuid->clone;
+
+Create a new UUID object with the identical value to the original.
+
+## as\_hex
+
+    my $hex = $uuid->as_hex;
+    my $hex = "$uuid";
+
+Returns the hex representation of the UUID.  The stringification of
+[UUID::FFI](https://metacpan.org/pod/UUID::FFI) uses this function, so you can also use it in a double quoted string.
+
+## compare
+
+    my $cmp = $uuid1->compare($uuid2);
+    my @sorted_uuids = sort { $a->compare($b) } @uuid;
+
+Returns an integer less than, equal to or greater than zero
+if `$uuid1` is found, respectively, to be lexicographically
+less than, equal, or greater that `$uuid2`.
+
+## type
+
+    my $type = $uuid->type;
+
+Returns the type of UUID, either `time` or `random`,
+if it can be identified.
+
+## variant
+
+    my $variant = $uuid->variant
+
+Returns the variant of the UUID, either `ncs`, `dce`, `microsoft` or `other`.
+
 # AUTHOR
 
 Graham Ollis <plicease@cpan.org>
--- lib/UUID/FFI.pm
+++ lib/UUID/FFI.pm
@@ -16,6 +16,19 @@ use overload fallback => 1;
 # ABSTRACT: Universally Unique Identifiers FFI style
 # VERSION
 
+=head1 SYNOPSIS
+
+ my $uuid = UUID::FFI->new_random;
+ say $uuid->as_hex;
+
+=head1 DESCRIPTION
+
+This module provides an FFI interface to C<libuuid>.
+C<libuuid> library is used to generate unique identifiers
+for objects that may be accessible beyond the local system
+
+=cut
+
 use constant {
   _lib => find_lib( lib => 'uuid' ),
   
@@ -81,6 +94,16 @@ use constant _variant => FFI::Raw->new(
   FFI::Raw::ptr,
 );
 
+=head1 CONSTRUCTORS
+
+=head2 new
+
+ my $uuid = UUID::FFI->new($hex);
+
+Create a new UUID object from the hex representation C<$hex>.
+
+=cut
+
 sub new
 {
   my($class, $hex) = @_;
@@ -91,6 +114,14 @@ sub new
   $self;
 }
 
+=head2 new_random
+
+ my $uuid = UUID::FFI->new_random;
+
+Create a new UUID object with a randomly generated value.
+
+=cut
+
 sub new_random
 {
   my($class) = @_;
@@ -98,6 +129,15 @@ sub new_random
   _generate_random->call($self);
   $self;
 }
+
+=head2 new_time
+
+ my $uuid = UUID::FFI->new_time;
+
+Create a new UUID object generated using the time and mac address.
+This can leak information about when and where the UUID was generated.
+
+=cut
 
 sub new_time
 {
@@ -107,6 +147,14 @@ sub new_time
   $self;
 }
 
+=head2 new_null
+
+ my $uuid = UUID::FFI->new_null;
+
+Create a new UUID C<NULL UUID>  object (all zeros).
+
+=cut
+
 sub new_null
 {
   my($class) = @_;
@@ -115,11 +163,29 @@ sub new_null
   $self;
 }
 
+=head1 METHODS
+
+=head2 is_null
+
+ my $bool = $uuid->is_null;
+
+Returns true if the UUID is C<NULL UUID>.
+
+=cut
+
 *is_null = FFI::Raw->new(
   _lib, 'uuid_is_null',
   FFI::Raw::int,
   FFI::Raw::ptr,
 )->coderef;
+
+=head2 clone
+
+ my $uuid2 = $uuid->clone;
+
+Create a new UUID object with the identical value to the original.
+
+=cut
 
 sub clone
 {
@@ -129,6 +195,16 @@ sub clone
   $other;
 }
 
+=head2 as_hex
+
+ my $hex = $uuid->as_hex;
+ my $hex = "$uuid";
+
+Returns the hex representation of the UUID.  The stringification of
+L<UUID::FFI> uses this function, so you can also use it in a double quoted string.
+
+=cut
+
 sub as_hex
 {
   my($self) = @_;
@@ -137,6 +213,17 @@ sub as_hex
   _unparse->call($self, $ptr);
   $data;
 }
+
+=head2 compare
+
+ my $cmp = $uuid1->compare($uuid2);
+ my @sorted_uuids = sort { $a->compare($b) } @uuid;
+
+Returns an integer less than, equal to or greater than zero
+if C<$uuid1> is found, respectively, to be lexicographically
+less than, equal, or greater that C<$uuid2>.
+
+=cut
 
 *compare = FFI::Raw->new(
   _lib, 'uuid_compare',
@@ -149,6 +236,15 @@ my %type_map = (
   4 => 'random',
 );
 
+=head2 type
+
+ my $type = $uuid->type;
+
+Returns the type of UUID, either C<time> or C<random>,
+if it can be identified.
+
+=cut
+
 sub type
 {
   my($self) = @_;
@@ -157,6 +253,14 @@ sub type
 }
 
 my @variant = qw( ncs dce microsoft other );
+
+=head2 variant
+
+ my $variant = $uuid->variant
+
+Returns the variant of the UUID, either C<ncs>, C<dce>, C<microsoft> or C<other>.
+
+=cut
 
 sub variant
 {
